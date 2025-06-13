0c394fb1800b48f1be0f7f69dbeaaa41e016ae33
Gabriel Serrano Rojas <gabriel.serranorojas@ucr.ac.cr>
2025-06-13 14:46:26 +0600

    Implemented printOutput().

--- tareas/tarea_programada_1/src/OutputHandler.cc
+++ tareas/tarea_programada_1/src/OutputHandler.cc
@@ -1,1 +1,38 @@
 #include "OutputHandler.h"
+
+#include <iomanip>
+#include <sstream>
+
+#include "Common.h"
+#include "Log.h"
+
+void OutputHandler::printOutput(Request& request) {
+  Log::getInstance().log("INFO",
+                         "OutputHandler::printOutput(): Printing output.");
+
+  std::stringstream outputStream;
+  outputStream << "------------------------------------------------------------"
+                  "------------------\n";
+  outputStream << "| " << std::left << std::setw(24) << "Subnet Name"
+               << "| " << std::setw(23) << "Subnet Address"
+               << "| " << std::setw(16) << "Total Addresses"
+               << "| " << std::setw(9) << "Mask"
+               << "|\n";
+  outputStream << "------------------------------------------------------------"
+                  "---------------------\n";
+
+  for (const auto& result : request.subnetResults) {
+    std::string subnetName = std::get<0>(result);
+    uint32_t subnetAddress = std::get<1>(result);
+    size_t subnetMask = std::get<2>(result);
+
+    outputStream << "| " << std::setw(24) << subnetName << "| " << std::setw(23)
+                 << Common::addressToString(subnetAddress) << "| "
+                 << std::setw(16) << (1 << (32 - subnetMask)) - 2 << "| "
+                 << std::setw(9) << subnetMask << "|\n";
+  }
+  outputStream << "------------------------------------------------------------"
+                  "---------------------\n";
+
+  Log::getInstance().log("RESULTS", outputStream.str());
+}
