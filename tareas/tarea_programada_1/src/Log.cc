60eca6c9a14317ec467b7c0e9b21a88f1d83c604
Gabriel Serrano Rojas <gabriel.serranorojas@ucr.ac.cr>
2025-06-13 10:18:31 +0600

    Moved to src. Logs created in /logs directory.

---
+++ tareas/tarea_programada_1/src/Log.cc
@@ -0,0 +1,35 @@
+#include "Log.h"
+#include <filesystem>
+
+Log& Log::getInstance() {
+  static Log instance;  // Guaranteed to be destroyed, instantiated on first use
+  return instance;
+}
+
+void Log::log(const std::string& level, const std::string& message) {
+  std::filesystem::path logDir("../logs");
+  if (!std::filesystem::exists(logDir)) {
+    std::filesystem::create_directory(logDir);
+  }
+  
+  std::string filename = "../logs/" + level + "_log.txt";
+  std::ofstream logFile(filename, std::ios::app);
+  if (logFile.is_open()) {
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    logFile << dt << " - " << message << std::endl;
+    logFile.close();
+  }
+}
+
+void Log::printLogs(const std::string& level) {
+  std::string filename = "../logs/" + level + "_log.txt";
+  std::ifstream logFile(filename);
+  if (logFile.is_open()) {
+    std::string line;
+    while (std::getline(logFile, line)) {
+      std::cout << line << std::endl;
+    }
+    logFile.close();
+  }
+}
