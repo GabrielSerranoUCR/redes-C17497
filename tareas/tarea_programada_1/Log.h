34a021b2730d028e04a99684987e6536ca501e66
Gabriel Serrano Rojas <gabriel.serranorojas@ucr.ac.cr>
2025-06-13 10:14:53 +0600

    Implemented logging service.

--- tareas/tarea_programada_1/Log.h
+++ tareas/tarea_programada_1/Log.h
@@ -1,5 +1,9 @@
 #ifndef LOG_H
 #define LOG_H
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 /*
 * @class Log
@@ -7,9 +11,20 @@
 
 * This class provides static methods for logging messages at different levels
 * (info, warning, error) to the console. It is designed to be used throughout
-* the application for consistent logging practices. It is designed to be a singleton.
+* the application for consistent logging practices. It is designed to be a
+singleton.
 */
 class Log {
+ private:
+  Log() = default;           // Private constructor to prevent instantiation
+  Log(const Log&) = delete;  // Prevent copy construction
 
+ public:
+  // Static method to get the singleton instance of Log
+  static Log& getInstance();
+
+  void log(const std::string& level, const std::string& message);
+
+  void printLogs(const std::string& level);
 };
-#endif // LOG_H
+#endif  // LOG_H
